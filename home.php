e346634c0491af04381700ead2ac32706ed585c5
TSDenley <tsdenley@gmail.com>
2012-09-16 17:50:11 +0100

    Added 'home.php' and custom reorder plugin

---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The home page template file.
+ *
+ * @package _tsd
+ * @since _tsd 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary" class="site-content">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php 
+					$args = array(
+					              'orderby'   => 'menu_order',
+					              'order'     => 'ASC',
+					              'showposts' => 9
+					            );
+
+					$my_query = new WP_Query($args); 
+				?>
+
+				<?php // _tsd_content_nav( 'nav-above' ); ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+				<?php // _tsd_content_nav( 'nav-below' ); ?>
+
+			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+
+				<?php get_template_part( 'no-results', 'index' ); ?>
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary .site-content -->
+
+<?php get_footer(); ?>
